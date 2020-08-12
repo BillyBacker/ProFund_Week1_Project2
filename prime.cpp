194140aac0606cc2282d05afb93aff00892aa146
BillyBacker <38096723+BillyBacker@users.noreply.github.com>
2020-08-12 10:02:27 +0700

    prime check now wil return all prime number in range.

---
+++ prime.cpp
@@ -0,0 +1,37 @@
+#include<stdio.h>
+bool prime(int In);
+
+int main() {
+	int min, max;
+	int i = 1;
+	printf("Enter minimum Number : ");
+	scanf_s("%d", &min);
+	printf("Enter maximum Number : ");
+	scanf_s("%d", &max);
+	for (int num = min; num < max; num++) {
+		if (prime(num)){
+			printf("%d is Prime\n", num);
+		}
+	}
+}
+
+bool prime(int In) {
+	int i = 1;
+	while (true)
+	{
+		if (In > i + 1 || In ==1) {
+			if (In == 1 || In % (In - i) == 0) {
+				return false;
+				break;
+			}
+			else {
+				i += 1;
+			}
+		}
+		else
+		{
+			return true;
+			break;
+		}
+	}
+}
