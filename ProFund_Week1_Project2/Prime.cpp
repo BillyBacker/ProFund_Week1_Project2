9978feab4096e87e75128a3fd4dc7e76b64b1350
BillyBacker <38096723+BillyBacker@users.noreply.github.com>
2020-08-12 09:34:03 +0700

    Create Prime number checker.

--- ProFund_Week1_Project2/Prime.cpp
+++ ProFund_Week1_Project2/Prime.cpp
@@ -1,20 +1,25 @@
 #include<stdio.h>
 
 int main() {
-	int In, i;
+	int In = 0;
+	int i = 1;
 	printf("Enter Number : ");
-	scanf_s("%d", In);
+	scanf_s("%d", &In);
 	while (true)
 	{
-		if (In > i) {
+		if (In > i+1) {
 			if (In % (In - i) == 0) {
 				printf("Not Prime");
 				break;
 			}
 			else {
-				printf("Prime");
-				break;
+				i += 1;
 			}
+		}
+		else
+		{
+			printf("Prime");
+			break;
 		}
 	}
 
